c3a23fbb5f7ef1aa4b1860bdee8b275c5c0ca855
Cyrill <cschleinkofe@student.ethz.ch>
2026-09-03 11:39:06 +0200

    fix: resolve builtin-modules and pnpm engine

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import esbuild from 'esbuild';
 import process from 'process';
-import { builtinModules } from 'node:module';
+import { builtinModules } from "node:module";
 import fs from "fs";
 import path from "path";
 
@@ -63,7 +63,7 @@ const context = await esbuild.context({
 		'@lezer/common',
 		'@lezer/highlight',
 		'@lezer/lr',
-		...builtinModules,
+		...builtinModules.flatMap((m) => [m, `node:${m}`]),
 	],
 	format: 'cjs',
 	target: 'es2021',
